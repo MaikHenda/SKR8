e0002058a89dca1afe3f4d30aaa7518b71df91ae
MaikHenda <maikhenckens@live.nl>
2021-01-17 20:39:54 +0000

    Added new files

---
+++ public/firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCyLZ_IkVWuvVioIo0lOgt1RLQf6WVUTN0",
+    authDomain: "skr8-daf74.firebaseapp.com",
+    databaseURL: "https://skr8-daf74.firebaseio.com",
+    projectId: "skr8-daf74",
+    storageBucket: "skr8-daf74.appspot.com",
+    messagingSenderId: "803590686527",
+    appId: "1:803590686527:web:32ea6574661aba8faa758a",
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
